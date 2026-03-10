5cf7c2af849ede4180b59cbf88299b9d8bf0cce2
Benson <BensonLin9801@gmail.com>
2026-03-10 02:16:31 -0400

    login in progress

--- ATMSystem/Program.cs
+++ ATMSystem/Program.cs
@@ -1,24 +1,56 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 using MySql.Data.MySqlClient;
 
 class Program
 {
+
+    string connString = "server=localhost;database=atm_system;uid=root;pwd=74327huHUFDdsf&&98767";
+
     static void Main()
     {
-        string connString = "server=localhost;database=atm_system;uid=root;pwd=74327huHUFDdsf&&98767";
+        Console.WriteLine("ATM SYSTEM LOGIN SCREEN");
 
-        MySqlConnection conn = new MySqlConnection(connString);
+        Console.Write("Enter login info: ");
+        string login = Console.ReadLine();
+
+        Console.Write("Enter PIN: ");
+        string pin = Console.ReadLine();
+
+        Login(login, pin);
+
+    }
+
+    static void Login(string login, string pin)
+    {
 
         try
         {
+            using MySqlConnection conn = new MySqlConnection(connString);
             conn.Open();
-            Console.WriteLine("Database Connected Successfully");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Connection Failed: " + ex.Message);
+
+            string query = "SELECT role, holder_name FROM accounts WHERE login=l@login and pin=@pin";
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@login", login);
+            cmd.Parameters.AddWithValue("@pin", pin);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+
+                string role = reader.GetString("role");
+                string name = reader.GetString("holder_name");
+
+
+            }
+
         }
 
-        Console.ReadLine();
+
+
+
     }
+
 }
