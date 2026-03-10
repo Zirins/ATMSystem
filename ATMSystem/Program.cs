8133617f0011e1a3d004b3e2168f39c01be31f3e
Benson <BensonLin9801@gmail.com>
2026-03-10 02:07:42 -0400

    Add project files.

---
+++ ATMSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connString = "server=localhost;database=atm_system;uid=root;pwd=74327huHUFDdsf&&98767";
+
+        MySqlConnection conn = new MySqlConnection(connString);
+
+        try
+        {
+            conn.Open();
+            Console.WriteLine("Database Connected Successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Connection Failed: " + ex.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
